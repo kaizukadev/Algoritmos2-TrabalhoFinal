9f331265a2b1440384546a8fa04dbced172871f9
Sérgio Ferreira <sergioferreira.ads@gmail.com>
2017-12-11 12:11:54 -0200

    Under Construction

--- src/datastructures/ListaEncadeada.java
+++ src/datastructures/ListaEncadeada.java
@@ -4,17 +4,46 @@ import java.io.FileReader;
 import java.util.Comparator;
 
 public class ListaEncadeada<T> {
-	public void append(T dado)
-	{}
-	public void addFirst(T dado)
-	{}
+	private Node<T> head;
+	private Node<T> tail;
+
+	public ListaEncadeada() {
+		this.head = null;
+		this.tail = null;
+	}  
+
+	public boolean isEmpty() {
+		return this.head == null && this.tail == null;   
+	}
+
+	public void append(T dado) {
+		Node<T> newNode = new Node<T>(dado, this.tail, null);
+		if(isEmpty()) {
+			this.head = newNode;
+		} else {
+		tail.setNext(newNode);
+		}
+		this.tail = newNode;
+	}
+	
+	public void addFirst(T dado) {
+		Node<T> newNode = new Node<T>(dado, null, this.head);
+		if(isEmpty()) {
+			this.tail = newNode;
+		} else {
+		head.setPrevious(newNode);
+		}
+		this.head = newNode;
+
+	}
+	
 	public T search(T key, Comparator<T> cmp)
 	{
 		return null;
 	}
 	public void printObjects()
 	{}
-	
+
 	public static ListaEncadeada<model.Aluno> loadFromFile(FileReader arquivo)
 	{
 		return null;
--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -1,5 +1,59 @@
 package model;
 
 public class Aluno {
+	private String matricula;
+	private String nome;
+	private String email;
+	private int    idade;
+	private String sexo;
+	private String empresa;
+	private String cidade;
 
+	public Aluno(String matricula, String nome, String email, int idade, String sexo, String empresa, String cidade) {
+		this.matricula = matricula;
+		this.nome = nome;
+		this.email = email;
+		this.idade = idade;
+		this.sexo = sexo;
+		this.empresa = empresa;
+		this.cidade = cidade;
+	}
+
+	public Aluno(String nome, String email, int idade, String cidade) {
+		this.matricula = "";
+		this.nome = nome;
+		this.email = email;
+		this.idade = idade;
+		this.sexo = "";
+		this.empresa = "";
+		this.cidade = cidade;
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public String getEmpresa() {
+		return empresa;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
 }
